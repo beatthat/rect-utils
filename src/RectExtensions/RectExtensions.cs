de39d285912ddd684976e1842520f75388f46a18
larrykirschner <larrykirschner@gmail.com>
2017-09-01 23:12:38 -0700

    0.0.1

---
+++ src/RectExtensions/RectExtensions.cs
@@ -0,0 +1,402 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Assertions;
+
+namespace BeatThat
+{
+	public enum RectConstraintsAdjustment { NONE = 0, ADJUSTED_TO_MEET_CONSTRAINTS = 1, ADJUSTED_BUT_FAILED_TO_MEET_CONSTRAINTS = 2 }
+
+	[Flags]
+	public enum EdgeFlags
+	{
+		None = 0, Left = 1, Top = 2, Right = 4, Bottom = 8
+	}
+
+	public static class RectExtensions 
+	{
+		public static Rect LerpTo(this Rect r, Rect r2, float pct)
+		{            
+			pct = Mathf.Clamp01(pct);
+
+			return new Rect(Mathf.Lerp(r.x, r2.x, pct), Mathf.Lerp(r.y, r2.y, pct),
+				Mathf.Lerp(r.width, r2.width, pct), Mathf.Lerp(r.height, r2.height, pct));
+		}
+
+		public static Rect LerpToUnclamped(this Rect r, Rect r2, float pct)
+		{           
+			return new Rect(Mathf.LerpUnclamped(r.x, r2.x, pct), Mathf.LerpUnclamped(r.y, r2.y, pct),
+				Mathf.LerpUnclamped(r.width, r2.width, pct), Mathf.LerpUnclamped(r.height, r2.height, pct));
+		}
+
+		public static float Area(this Rect r)
+		{            
+			return r.width * r.height;
+		}
+
+		public static bool Contains(this Rect r, Rect r2)
+		{            
+			return (r.Contains(r2.min) && r.Contains(r2.max));
+		}
+
+		public static Rect ToRect(this Vector4 v)
+		{
+			return new Rect(v.x, v.y, v.z, v.w);
+		}
+
+		/// <summary>
+		/// Get the corners of the rect: [bottomLeft, topLeft, topRight, bottomRight]
+		/// </summary>
+		/// <param name="corners">Corners.</param>
+		public static void GetCorners(this Rect r, Vector2[] corners)
+		{
+			Assert.AreEqual(4, corners.Length);
+
+			corners[0] = r.min;
+			corners[1] = new Vector2(r.xMin, r.yMax);
+			corners[2] = r.max;
+			corners[3] = new Vector2(r.xMax, r.yMin);
+		}
+
+		/// <summary>
+		/// Gets the edges (if any) that are colinear with point p. 
+		/// For the case of a corner, returns both adjacent edges, e.g. topRight = EdgeFlags.Top | EdgeFlags.Right
+		/// </summary>
+		/// <returns>The edges.</returns>
+		/// <param name="r">The rect.</param>
+		/// <param name="p">The position to test</param>
+		public static EdgeFlags GetEdges(this Rect r, Vector2 p)
+		{
+			var edges = EdgeFlags.None;
+
+			if(Mathf.Approximately(p.x, r.xMin)) {
+				edges |= EdgeFlags.Left;
+			}
+
+			if(Mathf.Approximately(p.x, r.xMax)) {
+				edges |= EdgeFlags.Right;
+			}
+
+			if(Mathf.Approximately(p.y, r.yMax)) {
+				edges |= EdgeFlags.Top;
+			}
+
+			if(Mathf.Approximately(p.y, r.yMin)) {
+				edges |= EdgeFlags.Bottom;
+			}
+
+			return edges;
+		}
+
+		/// <summary>
+		/// Calculate a Rect in viewport coordinates of a second Rect
+		/// </summary>
+		/// <returns>The Rect in viewport coordinates</returns>
+		/// <param name="r">The 'this' Rect calling the extension function to be converted into viewport coordinates</param>
+		/// <param name="r2">The rect to use as the reference</param>
+		public static Rect InViewportCoordinatesOf(this Rect r, Rect r2)
+		{
+			if(r2.width <= 0 || r2.height <= 0f) {
+				Debug.LogWarning("[" + Time.frameCount + "]RectExtensions::InViewportCoordinatesOf r2 is collapsed: " + r2);
+				return new Rect(0f, 0f, 1f, 1f);
+			}
+
+			return new Rect((r.x - r2.x) / r2.width, (r.y - r2.y) / r2.height,
+				r.width / r2.width, r.height / r2.height);
+		}
+
+		/// <summary>
+		/// Bounds-type function applied to Rect: encapsulates a rect so that the returned Rect contains r and r2.
+		/// </summary>
+		public static Rect Encapsulate(this Rect r, Rect r2)
+		{
+			var res = new Rect();
+			res.xMin = Mathf.Min(r.xMin, r2.xMin);
+			res.yMin = Mathf.Min(r.yMin, r2.yMin);
+			res.xMax = Mathf.Max(r.xMax, r2.xMax);
+			res.yMax = Mathf.Max(r.yMax, r2.yMax);
+			return res;
+		}
+
+		public static float GetAspect(this Rect r)
+		{
+			if(r.height <= 0) {
+				Debug.LogWarning("[" + Time.frameCount + "] RectExtensions::GetAspect height is not positive");
+				return float.NaN;
+			}
+
+			return r.width/r.height;
+		}
+
+		public static bool Intersects(this Rect r, Rect r2, out Rect intersection)
+		{
+			if(r.Covers(r2)) {
+				intersection = r2;
+				return true;
+			}
+
+			if(r2.Covers(r)) {
+				intersection = r;
+				return true;
+			}
+
+			if(r.Overlaps(r2)) {
+				intersection = Rect.MinMaxRect(
+					Mathf.Max(r.xMin, r2.xMin),
+					Mathf.Max(r.yMin, r2.yMin),
+					Mathf.Min(r.xMax, r2.xMax),
+					Mathf.Min(r.yMax, r2.yMax));
+
+				return true;
+			}
+
+			intersection = new Rect();
+			return false;
+		}
+		
+
+		/// <summary>
+		/// Like contains except allowing covered (contained) rect to share an edge with the covering/containing rect.
+		/// </summary>
+		public static bool Covers(this Rect r, Rect r2, float epsilon = 0.0001f)
+		{          
+			if(r2.xMin < r.xMin - epsilon) {
+				return false;
+			}
+
+			if(r2.xMax > r.xMax + epsilon) {
+				return false;
+			}
+
+			if(r2.yMin < r.yMin - epsilon) {
+				return false;
+			}
+
+			if(r2.yMax > r.yMax + epsilon) {
+				return false;
+			}
+
+			return true;
+		}
+
+		public static bool Covers(this Rect r, Vector2 point, float epsilon = 0.0001f)
+		{          
+			if(point.x < r.xMin - epsilon) {
+				return false;
+			}
+
+			if(point.x > r.xMax + epsilon) {
+				return false;
+			}
+
+			if(point.y < r.yMin - epsilon) {
+				return false;
+			}
+
+			if(point.y > r.yMax + epsilon) {
+				return false;
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// If the point p is outside the rect, returns the closest point on the rect.
+		/// NOTE this assumes the rect is axis aligned
+		/// </summary>
+		/// <param name="r">The rect to clamp to</param>
+		/// <param name="p">The point</param>
+		public static bool Clamp(this Rect r, ref Vector3 p)
+		{
+			if(r.Contains(p)) {
+				return false;
+			}
+				
+			var center = (Vector3)r.center;
+			center.z = p.z;
+
+			var b = new Bounds(center, new Vector3(r.width, r.height, 0.00001f));
+			p = b.ClosestPoint(p);
+			return true;
+		}
+
+		/// <summary>
+		/// Check if a line (given by endpoints) intersects a rect.
+		/// For these purposes, if the line is fully contained within the rect will be considered intersecting.
+		/// Returns the resulting contained/intersecting line via out params
+		/// </summary>
+		/// <returns><c>true</c>, if line was intersectsed, <c>false</c> otherwise.</returns>
+		/// <param name="r">The rect</param>
+		/// <param name="p1">line end point 1</param>
+		/// <param name="p2">line end point 2</param>
+		/// <param name="p1WithinRect">result line endpoint 1</param>
+		/// <param name="p2WithinRect">result line endpoint 2</param>
+		public static bool IntersectsLine(this Rect r, Vector3 p1, Vector3 p2, out Vector3 p1WithinRect, out Vector3 p2WithinRect)
+		{
+			p1WithinRect = p1;
+			p2WithinRect = p2;
+
+			var p1Inside = r.Contains(p1);
+			var p2Inside = r.Contains(p2);
+
+			if(p1Inside && p2Inside) {
+				return true;
+			}
+
+			var intersects = false;
+
+			var len = Vector2.Distance((Vector2)p1, (Vector2)p2);
+
+			float dist;
+			var b = new Bounds(r.center, new Vector3(r.width, r.height, 0.0001f));
+
+			if(!p1Inside) {
+				if(b.IntersectRay(new Ray((Vector2)p1, (Vector2)(p2 - p1)), out dist) && dist <= len) {
+					p1WithinRect = Vector3.Lerp(p1, p2, dist/len);
+					intersects = true;
+				}
+			}
+
+			if(!p2Inside) {
+				if(b.IntersectRay(new Ray((Vector2)p2, (Vector2)(p1 - p2)), out dist) && dist <= len) {
+					p2WithinRect = Vector3.Lerp(p2, p1, dist/len);
+					intersects = true;
+				}
+			}
+
+			return intersects;
+		}
+
+		public static bool IsOnEdge(this Rect r, Vector2 point, float epsilon = 0.0001f)
+		{          
+			if(Mathf.Abs(point.x - r.xMin) <= epsilon) {
+				return true;
+			}
+
+			if(Mathf.Abs(point.x - r.xMax) <= epsilon) {
+				return true;
+			}
+
+			if(Mathf.Abs(point.y - r.yMin) <= epsilon) {
+				return true;
+			}
+
+			if(Mathf.Abs(point.y - r.yMax) <= epsilon) {
+				return true;
+			}
+
+			return false;
+		}
+
+
+		public static bool Approximately(this Rect r, Rect r2, float epsilon = 0.0001f)
+		{
+			if(Mathf.Abs(r.x - r2.x) > epsilon) {
+				return false;
+			}
+
+			if(Mathf.Abs(r.y - r2.y) > epsilon) {
+					return false;
+			}
+
+			if(Mathf.Abs(r.width - r2.width) > epsilon) {
+					return false;
+			}
+
+			if(Mathf.Abs(r.height - r2.height) > epsilon) {
+					return false;
+			}
+
+			return true;
+		}
+
+
+		/// <summary>
+		/// Clamp Rect r2 to Rect r1 in a way that ensures their intersection meets min width and min height constraints.
+		/// In cases where the intersection of the two rects does NOT meet the constraints, 
+		/// tries to adjust the intersection to find the rect within bounds closest to the original Rect r that meets the constraints.
+		/// </summary>
+		/// <param name="r">the 'calling' rect</param>
+		/// <param name="bounds">The bounds rect into which the final intersection must fit</param>
+		/// <param name = "intersect">The out param intersection result</param>
+		/// <param name="minWidth">Minimum width constraint for the intersection</param>
+		/// <param name="minHeight">Minimum height constraint for the intersection</param>
+		/// <returns>
+		/// RectConstraintsAdjustment.NONE if no adjustment was necessary, 
+		/// RectConstraintsAdjustment.ADJUSTED_TO_MEET_CONSTRAINTS if the intersection was adjusted and now meets the constraints,
+		/// RectConstraintsAdjustment.ADJUSTED_BUT_FAILED_TO_MEET_CONSTRAINTS if the intersection was adjusted but still fails to meet constraints (bounds themselves do not meet constraints)
+		/// </returns>
+		public static RectConstraintsAdjustment ClosestIntersectionThatMeetsConstraints(this Rect r, Rect bounds, out Rect intersect, float minWidth, float minHeight)
+		{
+			if(!r.Intersects(bounds, out intersect)) {
+				// no intersection so return the slice of bounds that is closest to Rect r and that meets min-width/min-height constraints
+
+				intersect = r;
+
+				if(intersect.xMin >= bounds.xMax) {
+					intersect.xMin = bounds.xMax - minWidth;
+					intersect.xMax = bounds.xMax;
+				}
+				else if(intersect.xMax <= bounds.xMin) {
+					intersect.xMin = bounds.xMin;
+					intersect.xMax = bounds.xMin + minWidth;
+				}
+
+				if(intersect.yMin >= bounds.yMax) {
+					intersect.yMin = bounds.yMax - minHeight;
+					intersect.yMax = bounds.yMax;
+				}
+				else if(r.yMax <= bounds.yMin) {
+					intersect.yMin = bounds.yMin;
+					intersect.yMax = bounds.yMin + minHeight;
+				}
+
+				return RectConstraintsAdjustment.ADJUSTED_TO_MEET_CONSTRAINTS;
+			}
+
+			if(intersect.width >= minWidth && intersect.height >= minHeight) {
+				return RectConstraintsAdjustment.NONE; 
+			}
+
+			if(intersect.width < minWidth) {
+				if(Mathf.Approximately(intersect.xMin, bounds.xMin)) {
+					intersect.xMax = bounds.xMin + minWidth;
+				}
+				else if(Mathf.Approximately(intersect.xMax, bounds.xMax)) {
+					intersect.xMin = bounds.xMax - minWidth;
+				}
+				else {
+					// intersect is fully within bounds in the x axis, but just too small...
+					if(Mathf.Abs(intersect.xMin - bounds.xMin) < Mathf.Abs(bounds.xMax - intersect.xMax)) { 
+						intersect.xMin = Mathf.Clamp(intersect.xMax - minWidth, bounds.xMin, bounds.xMax);
+						if(intersect.width < minWidth) { // still too small, try setting xMax to bounds.xMax
+							intersect.xMax = bounds.xMax;
+						}
+					}
+				}
+			}
+
+			if(intersect.height < minHeight) {
+				if(Mathf.Approximately(intersect.yMin, bounds.yMin)) {
+					intersect.yMax = bounds.yMin + minHeight;
+				}
+				else if(Mathf.Approximately(intersect.yMax, bounds.yMax)) {
+					intersect.yMin = bounds.yMax - minHeight;
+				}
+				else {
+					// intersect is fully within bounds in the y axis, but just too small...
+					if(Mathf.Abs(intersect.yMin - bounds.yMin) < Mathf.Abs(bounds.yMax - intersect.yMax)) { 
+						intersect.xMin = Mathf.Clamp(intersect.xMax - minWidth, bounds.xMin, bounds.xMax);
+						if(intersect.height < minHeight) { // still too small, try setting yMax to bounds.yMax
+							intersect.yMax = bounds.yMax;
+						}
+					}
+				}
+			}
+
+			return intersect.width >= minWidth && intersect.height >= minHeight? 
+				RectConstraintsAdjustment.ADJUSTED_TO_MEET_CONSTRAINTS: RectConstraintsAdjustment.ADJUSTED_BUT_FAILED_TO_MEET_CONSTRAINTS;
+
+		}
+
+	}
+}
---
+++ src/RectExtensions/RectTransformExtensions.cs
@@ -0,0 +1,399 @@
+﻿using UnityEngine;
+using BeatThat;
+using UnityEngine.Assertions;
+
+namespace BeatThat
+{
+	public static class RectTransformExtensions 
+	{
+		#if UNITY_EDITOR
+		public static void DrawGizmoScreenRect(this RectTransform rt, Rect screenRect, Color color)
+		{
+			var saveColor = Gizmos.color;
+
+			var r = rt.TransformScreenRect(screenRect);
+
+			Gizmos.color = color;
+
+			Gizmos.DrawLine((Vector3)r.min, new Vector3(r.xMin, r.yMax));
+			Gizmos.DrawLine(new Vector3(r.xMin, r.yMax), (Vector3)r.max);
+			Gizmos.DrawLine((Vector3)r.max, new Vector3(r.xMax, r.yMin));
+			Gizmos.DrawLine(new Vector3(r.xMax, r.yMin), (Vector3)r.min);
+
+			Gizmos.color = saveColor;
+		}
+
+		public static void DrawGizmoFillScreenRect(this RectTransform rt, Rect screenRect, Color color)
+		{
+			var saveColor = Gizmos.color;
+
+			var r = rt.TransformScreenRect(screenRect);
+
+			Gizmos.color = color;
+
+			Gizmos.DrawCube(r.center, r.size);
+
+			Gizmos.color = saveColor;
+		}
+		#endif
+
+		public static bool ScreenPointToWorldPoint(this RectTransform rt, Vector2 screenPoint, out Vector3 worldPoint, Canvas canvas = null) 
+		{
+			if(canvas == null) {
+				canvas = rt.GetComponentInParent<Canvas>();
+			}
+
+			if (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace) {
+				return RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, screenPoint, canvas.worldCamera, out worldPoint);
+			}
+			else {
+				return RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, screenPoint, null, out worldPoint);
+			}
+		}
+
+		/// <summary>
+		/// Gets the aspect ratio (width/height) of the screen rect for a RectTransform
+		/// </summary>
+		public static float GetScreenAspect(this RectTransform rt, Canvas canvas = null) 
+		{
+			var screenRect = rt.GetScreenRect(canvas);
+
+			if(screenRect.height <= 0f) {
+				Debug.LogWarning("[" + Time.frameCount + "] RectTransformExtensions::GetScreenAspect height is not a positive number");
+				return float.NaN;
+			}
+
+			return screenRect.width/screenRect.height;
+		}
+
+		public static bool ContainsScreenPoint(this RectTransform rt, Vector2 screenPoint)
+		{
+			return RectTransformUtility.RectangleContainsScreenPoint(rt, screenPoint);
+		}
+
+		/// <summary>
+		/// Changes a RectTransform to assume the screen rect of its target
+		/// and then also make the target its child with anchors pinned to the corners.
+		/// 
+		/// This is useful when a component wants to take ownership of some external object
+		/// and transition it into place.
+		/// </summary>
+		/// <param name="thisRt">This rt.</param>
+		/// <param name="target">Target.</param>
+		public static void BecomeParentAndDriverOf(this RectTransform thisRt, RectTransform target)
+		{
+			thisRt.SetScreenRect(target.GetScreenRect());
+			target.SetParent(thisRt.transform, true);
+			target.anchorMin = Vector2.zero;
+			target.anchorMax = Vector2.one;
+			target.offsetMin = Vector2.zero;
+			target.offsetMax = Vector2.zero;
+		}
+			
+		/// <summary>
+		/// Gets the screen rect (pixel rect) for a RectTransform.
+		/// 
+		/// NOTE: does NOT work if rect has rotation applied
+		/// 
+		/// </summary>
+		/// <returns>The screen rect.</returns>
+		/// <param name="rt">Rect transform.</param>
+		/// <param name="canvas">Canvas.</param>
+		public static Rect GetScreenRect(this RectTransform rt, Canvas canvas = null) 
+		{
+			if(rt.eulerAngles != Vector3.zero) {
+				#if UNITY_EDITOR || BT_DEBUG_UNSTRIP
+				Debug.LogWarning("[" + Time.frameCount + "][" + rt.Path() + "] GetScreenRect does not work on rotate RectTransform eulerAngles=" + rt.eulerAngles);
+				#endif
+			}
+
+			if(canvas == null) {
+				canvas = rt.GetComponentInParent<Canvas>();
+			}
+
+			using(var cornersArr = ArrayPool<Vector3>.Get(4)) {
+				using(var screenCornersArr = ArrayPool<Vector3>.Get(2)) {
+					
+					var corners = cornersArr.array;
+					var screenCorners = screenCornersArr.array;
+
+					rt.GetWorldCorners(corners);
+
+					if (canvas != null && (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace)) {
+						screenCorners[0] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[0]);
+						screenCorners[1] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[2]);
+					}
+					else {
+						screenCorners[0] = RectTransformUtility.WorldToScreenPoint(null, corners[0]);
+						screenCorners[1] = RectTransformUtility.WorldToScreenPoint(null, corners[2]);
+					}
+
+					return new Rect(screenCorners[0], screenCorners[1] - screenCorners[0]);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Update the anchors of a RectTransform without changing its screen rect
+		/// </summary>
+		/// <param name="anchorMax">Anchor max.</param>
+		public static void SetAnchorsWithCurrentScreenRect(this RectTransform rt, Vector2 anchorMin, Vector2 anchorMax)
+		{
+			var screenRect = rt.GetScreenRect();
+
+			if(anchorMax.x - anchorMin.x < .01f || anchorMax.y - anchorMin.y < 0.1f) {
+				// avoid collapsing the rect by giving it a temp size
+				rt.sizeDelta = new Vector2(1f, 1f);
+			}
+
+
+			rt.anchorMax = anchorMax;
+			rt.anchorMin = anchorMin;
+
+			rt.SetScreenRect(screenRect);
+		}
+
+		/// <summary>
+		/// Gets the screen rect (pixel rect) for a RectTransform
+		/// </summary>
+		/// <returns>The screen rect.</returns>
+		/// <param name="rt">The extension-method 'this' RectTransform to which the new screen rect will be applied</param>
+		/// <param name = "tgtScreenRect">The screen coords to apply</param>
+		/// <param name="canvas">Canvas.</param>
+		public static void SetScreenRect(this RectTransform rt, Rect tgtScreenRect, Canvas canvas = null) 
+		{
+			if(canvas == null) {
+				canvas = rt.GetComponentInParent<Canvas>();
+			}
+
+
+			var tgtValid = (tgtScreenRect.width > 0f && tgtScreenRect.height > 0f);
+
+//			Assert.IsTrue(tgtValid, "[" + rt.Path() + "] attempt to set rect to collapsed or inverted shape (not supported)");
+
+			if(!tgtValid) {
+				Debug.LogWarning("[" + rt.Path() + "] attempt to set rect to collapsed or inverted shape (not supported)");
+				return;
+			}
+
+			var curScreenRect = rt.GetScreenRect(canvas);
+
+			var curValid = (curScreenRect.width > 0f && curScreenRect.height > 0f);
+//			Assert.IsTrue(curValid, "[" + rt.Path() + "] attempt to set rect to collapsed or inverted shape (not supported)");
+
+			if(!curValid) {
+				rt.sizeDelta = Vector2.one; // try to recover by giving rect a 1 unit size...
+				curScreenRect = rt.GetScreenRect(canvas);
+				curValid = (curScreenRect.width > 0f && curScreenRect.height > 0f);
+				if(!curValid) {
+					Debug.LogWarning("[" + rt.Path() + "] Not implemented to work oncollapsed or inverted rect " + curScreenRect);
+					return;
+				}
+			}
+
+			if(curScreenRect.Approximately(tgtScreenRect)) {
+				return;
+			}
+
+			var cam = (canvas != null && (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace))? canvas.worldCamera: null;
+
+			var scaleFactor = new Vector2(tgtScreenRect.width/curScreenRect.width, tgtScreenRect.height/curScreenRect.height);
+			if(scaleFactor != Vector2.one) {
+				rt.ScaleBy(scaleFactor, false);
+				curScreenRect = rt.GetScreenRect(canvas);
+			}
+
+			Vector2 curCenter, tgtCenter;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(rt.parent as RectTransform, tgtScreenRect.center, cam, out tgtCenter);
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(rt.parent as RectTransform, curScreenRect.center, cam, out curCenter);
+			var dPos = (Vector2)tgtCenter - (Vector2)curCenter;
+			rt.anchoredPosition += dPos;
+		}
+
+		/// <summary>
+		/// Scale a RectTransform by changing it's size (while leaving scale alone)
+		/// </summary>
+		/// <param name="rt">The rect transform ('this' in extension method)</param>
+		/// <param name = "scaleFactor">The factor by which to scale</param>
+		/// <param name = "adjustAnchoredPosition">If TRUE will adjust the anchored position of the RectTransform by the scaleFactor. Default is TRUE</param>
+		public static void ScaleBy(this RectTransform rt, Vector2 scaleFactor, bool adjustAnchoredPosition = true) 
+		{
+			var r = rt.rect;
+			rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, r.width * scaleFactor.x);
+			rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, r.height * scaleFactor.y);
+
+			if(adjustAnchoredPosition) {
+				rt.anchoredPosition = new Vector2(rt.anchoredPosition.x * scaleFactor.x, rt.anchoredPosition.y * scaleFactor.y);
+			}
+		}
+
+		/// <summary>
+		/// Scale a RectTransform by changing it's size (while leaving scale alone)
+		/// </summary>
+		/// <param name="rt">The rect transform ('this' in extension method)</param>
+		/// <param name = "scaleFactor">The factor by which to scale</param>
+		/// <param name = "adjustAnchoredPosition">If TRUE will adjust the anchored position of the RectTransform by the scaleFactor. Default is TRUE</param>
+		public static void ScaleBy(this RectTransform rt, float scaleFactor, bool adjustAnchoredPosition = true) 
+		{
+			rt.ScaleBy(new Vector2(scaleFactor, scaleFactor), adjustAnchoredPosition);
+		}
+
+		/// <summary>
+		/// Calculates the scale factor that, when applied, would produce the given world height,
+		/// e.g. if the current world height is 100 and the new world height is 50, then returns 0.5f
+		/// </summary>
+		/// <returns>The factor to world height.</returns>
+		/// <param name="rt">Rt.</param>
+		/// <param name="targetWorldHeight">World height.</param>
+		public static float ScaleFactorToWorldHeight(this RectTransform rt, float targetWorldHeight) 
+		{
+			var localHeight = Mathf.Abs(rt.InverseTransformVector(new Vector3(0f, targetWorldHeight, 0f)).magnitude);
+			return localHeight/rt.rect.height;
+		}
+
+		/// <summary>
+		/// Calculates the scale factor that, when applied, would produce the given screen height,
+		/// e.g. if the current screen height is 100 and the new world height is 50, then returns 0.5f
+		/// </summary>
+		/// <returns>The factor to screen height.</returns>
+		/// <param name="rt">Rt.</param>
+		/// <param name="tgtScreenHeight">screen height.</param>
+		public static float ScaleFactorToScreenHeight(this RectTransform rt, float tgtScreenHeight) 
+		{
+			var curHeight = rt.GetScreenRect().height;
+			return tgtScreenHeight/curHeight;
+		}
+
+		/// <summary>
+		/// Scale a RectTransform to a given world height by changing it's width and height (while leave actual scale alone)
+		/// </summary>
+		public static void ScaleToWorldHeight(this RectTransform rt, float targetWorldHeight) 
+		{
+			rt.ScaleBy(rt.ScaleFactorToWorldHeight(targetWorldHeight));
+		}
+
+		/// <summary>
+		/// Scale a RectTransform to a given screen height by changing it's width and height (while leave actual scale alone)
+		/// </summary>
+		public static void ScaleToScreenHeight(this RectTransform rt, float tgtScreenHeight) 
+		{
+			rt.ScaleBy(rt.ScaleFactorToScreenHeight(tgtScreenHeight));
+		}
+			
+		public static Rect GetWorldRect(this RectTransform rt) 
+		{
+			using(var cornersArr = ArrayPool<Vector3>.Get(4)) {
+				var corners = cornersArr.array;
+				rt.GetWorldCorners(corners);
+				return Rect.MinMaxRect(corners[0].x, corners[0].y, corners[2].x, corners[2].y);
+			}
+		}
+
+
+		/// <summary>
+		/// Makes a RectTransform fill the full space of another rect transform.
+		/// Mostly for creating test objects. 
+		/// </summary>
+		public static RectTransform FillSpaceOf(this RectTransform rt, RectTransform rt2)
+		{
+//			Debug.LogError("["+ Time.frameCount + "] RectTransform[" + rt.name + "]::FillSpace of " + rt2.name);
+
+//			var parentRect = r.InverseTransformRect(r.parent as RectTransform);
+			var tgtRect = rt.InverseTransformRect(rt2);
+
+			rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, tgtRect.width);
+			rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tgtRect.height);
+
+			var rect = rt.rect;
+			rt.anchoredPosition += new Vector2(tgtRect.xMin - rect.xMin, tgtRect.yMin - rect.yMin);
+			return rt;
+		}
+
+		/// <summary>
+		/// Makes a RectTransform fill the full space of its parent.
+		/// Mostly for creating test objects. 
+		/// </summary>
+		public static RectTransform FillParentSpace(this RectTransform r)
+		{
+			r.FillSpaceOf(r.parent as RectTransform);
+			return r;
+		}
+
+		/// <summary>
+		/// Variation of RectTransform::SetInsetAndSizeFromParentEdge that clamps the resulting edge
+		/// to ensure that it's contained within the parent
+		/// </summary>
+		public static void SetInsetAndSizeFromParentEdgeInterior(this RectTransform rt, RectTransform.Edge edge, float inset, float size)
+		{
+			var pSize = (edge == RectTransform.Edge.Bottom || edge == RectTransform.Edge.Top)? rt.rect.height: rt.rect.width;
+
+			// clamp the inset so that it is neither negative (past the parent edge) 
+			// nor greater than the parent's size (past the opposite parent edge)
+			var insetClamped = Mathf.Clamp(inset, 0f, pSize); 
+
+			// if the inset has been changed by clamping, adjust the size accordingly
+			// and also so that it is not greater than the parent size 
+			var sizeAdjusted = Mathf.Clamp(size - Mathf.Abs(inset - insetClamped), 0f, pSize);
+			rt.SetInsetAndSizeFromParentEdge(edge, insetClamped, sizeAdjusted);
+		}
+
+		public static Rect InverseTransformRect(this RectTransform rt, RectTransform rt2)
+		{
+			Rect r;
+			var c = rt.GetComponentInParent<Canvas>();
+			if (c == null || !c.pixelPerfect) {
+				r = rt2.rect;
+			}
+			else {
+				r = RectTransformUtility.PixelAdjustRect(rt2, c);
+			}
+
+			var p =  rt2.TransformPoint(new Vector2(r.x, r.y));
+			p = rt.InverseTransformPoint(p);
+
+			r.x = p.x;
+			r.y = p.y;
+
+			return r;
+		}
+
+		/// <summary>
+		/// Transform a rect from the ScreenSpace to world space
+		/// </summary>
+		/// <returns>The Rect in world coords</returns>
+		/// <param name="screenRect">Rect in screen coords/param>
+		public static Rect TransformScreenRect(this RectTransform rt, Rect screenRect, Canvas canvas = null)
+		{
+			if(canvas == null) {
+				canvas = rt.GetComponentInParent<Canvas>();
+			}
+
+			if(canvas == null) {
+				Debug.LogWarning("[" + Time.frameCount + "][" + rt.Path() + " unable to find parent canvas");
+			}
+
+			var cam = canvas != null && (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace)? canvas.worldCamera: null;
+
+			Vector2 localMin, localMax;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, screenRect.min, cam, out localMin);
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, screenRect.max, cam, out localMax);
+
+			return rt.TransformRect(Rect.MinMaxRect(localMin.x, localMin.y, localMax.x, localMax.y));
+		}
+
+		/// <summary>
+		/// Transform a rect from the RectTransform's coordinate space to world space
+		/// </summary>
+		/// <returns>The Rect in world coords</returns>
+		/// <param name="r">Rect in coord space of the RectTransform</param>
+		public static Rect TransformRect(this RectTransform rt, Rect r)
+		{
+			var min = rt.TransformPoint(r.min);
+			var max = rt.TransformPoint(r.max);
+			r.min = min;
+			r.max = max;
+			return r;
+		}
+
+	}
+}
